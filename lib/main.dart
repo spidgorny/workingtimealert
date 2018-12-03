9e9ddc65476f8b347f147e4e8df719a3f5a662cb
Slawa Pidgorny <spidgorny@gmail.com>
2018-12-03 14:50:18 +0100

    - add real time calculations

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,13 +30,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Timer timer;
+  DateTime now = DateTime.now();
+
+  _MyHomePageState() : super() {
+    timer = new Timer.periodic(new Duration(milliseconds: 10 * Duration.millisecondsPerSecond), (t) {
+      setState(() {
+        now = DateTime.now();
+      });
+    });
+  }
+
   @override
+  void initState() {
+    super.initState();
+  }
 
-  var now = DateTime.now();
-  var comeIn = new DateTime(now.year, now.month, now.day, 8, 33);
-  var breaks = Duration(minutes: 33);
+  static String _twoDigits(int n) {
+    if (n >= 10) return "${n}";
+    return "0${n}";
+  }
 
+  static String _time(DateTime comeIn) {
+    return _twoDigits(comeIn.hour) + ':' + _twoDigits(comeIn.minute);
+  }
+
+  static String _timeD(Duration comeIn) {
+    int minutes = comeIn.inMinutes.remainder(Duration.minutesPerHour);
+    return _twoDigits(comeIn.inHours) + ':' + _twoDigits(minutes);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var comeIn = new DateTime(now.year, now.month, now.day, 8, 34);
+    var breaks = Duration(minutes: 33);
+    var dur77 = Duration(hours: 7, minutes: 42);
+    var plus77 = comeIn.add(dur77);
+    plus77 = plus77.add(breaks);
+    var dur10 = Duration(hours: 10);
+    var plus10 = comeIn.add(dur10);
+    plus10 = plus10.add(breaks);
+    var working = DateTime.now().difference(comeIn);
+    var remain77 = dur77 - working;
+    var remain10 = dur10 - working;
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Working Time Alert'),
@@ -44,25 +83,28 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            new AnnotatedNumber("05:58", "Working Time"),
+            new AnnotatedNumber(_timeD(working), "Working Time Today"),
             new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  new AnnotatedNumber("08:55", "Come In", fontSize: 34),
-                  new AnnotatedNumber("17:05", "+ 7.7h", fontSize: 34),
-                  new AnnotatedNumber("18:55", "+ 10h", fontSize: 34),
+                  new AnnotatedNumber(_time(comeIn), "Come In", fontSize: 34),
+                  new AnnotatedNumber(_time(plus77), "+ 7.7h", fontSize: 34),
+                  new AnnotatedNumber(_time(plus10), "+ 10h", fontSize: 34),
                 ]),
             new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  new AnnotatedNumber("00:33", "Breaks", fontSize: 34),
-                  new AnnotatedNumber("02:05", "Remaining", fontSize: 34),
-                  new AnnotatedNumber("03:15", "Remaining", fontSize: 34),
-                ])
+                  new AnnotatedNumber(_timeD(breaks), "Breaks", fontSize: 34),
+                  new AnnotatedNumber(_timeD(remain77), "Remaining",
+                      fontSize: 34),
+                  new AnnotatedNumber(_timeD(remain10), "Remaining",
+                      fontSize: 34),
+                ]),
+            Text("Updated: " + DateTime.now().toString())
           ]),
     );
   }
